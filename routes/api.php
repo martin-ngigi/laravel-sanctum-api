2129a8545a9e6a895019216fedba8861de93afb0
martin-ngigi <martinwainaina001@gmail.com>
2022-12-14 08:46:29 +0300

    Posting And Getting product success

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,7 +20,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * get all products
+ */
 //http://127.0.0.1:8000/api/products
 Route::get('/products', function () {
-    return 'products';
+    return Product::all();
+});
+
+/**
+ * Post product
+ */
+Route::post('/products', function () {
+    return Product::create([
+        'name' => 'Product One',
+        'slug' => 'Product-one',
+        'description' => 'This is product description',
+        'price' => '99.99',
+    ]);
 });
