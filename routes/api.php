daacfd71cf64e37683b0a5d7b2739183561b0bc9
martin-ngigi <martinwainaina001@gmail.com>
2022-12-18 15:39:43 +0300

    replaced ? with / when searching for a product

--- routes/api.php
+++ routes/api.php
@@ -48,7 +48,7 @@ Route::post('/products',[ProductController::class, 'store']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-//http://127.0.0.1:8000/api/products/search?"name of product"
+//http://127.0.0.1:8000/api/products/search/"name of product"
 Route::get('/products/search/{name}', [ProductController::class,'search']);
 
 /**
